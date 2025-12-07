904fcc6f2997d37d5e192b3019f830b4e7b5b472
mbachel <mattbachscouts@gmail.com>
2025-12-07 15:16:41 -0500

    feat: finalizing rough draft of dashboard

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -43,6 +43,12 @@ export default function Dashboard() {
           <h3 className="pl-2 mb-2 text-2xl font-semibold">References</h3>
           <ul className="list-disc list-inside">
             <li className="pl-4">
+              CNovet, J., & Vanian, J. (2025, April 30). Satya Nadella says as much as 30% of Microsoft code is written by AI. CNBC. <br />
+              <Link to="https://www.cnbc.com/2025/04/29/satya-nadella-says-as-much-as-30percent-of-microsoft-code-is-written-by-ai.html" target="_blank" rel="noopener noreferrer" className="text-(--link) underline">
+                https://www.cnbc.com/2025/04/29/satya-nadella-says-as-much-as-30percent-of-microsoft-code-is-written-by-ai.html
+              </Link>
+            </li>
+            <li className="pl-4">
               Exploding Topics:&nbsp;
               <Link to="https://explodingtopics.com/blog/list-of-llms" target="_blank" rel="noopener noreferrer" className="text-(--link) underline">
                 Best 44 Large Language Models (LLMs) in 2025 (Exploding Topics, October 17th, 2025)
@@ -64,45 +70,73 @@ export default function Dashboard() {
         </div>
         {/*Topic Summary*/}
         <div className="max-w-7xl" id="summary">
-          <h3 className="pl-2 mb-2 text-2xl font-semibold">Summary</h3>
+          <h3 className="pl-2 mt-6 mb-2 text-2xl font-semibold">Summary</h3>
           <p className="pl-4 mb-2 text-lg">
-            The content on this site...
+            Artificial Intelligence has been one of the biggest topics in technology today. 
+            In particular, development and deployment of Generative AI has seen rapid growth.
+            Companies such as Anthropic, Google, and OpenAI have been leading the way in this 
+            AI renaissance. Recently, these companies have released powerful models, Claude 4.5,
+            Gemini 3 Pro, and GPT-5 respectively. Along with these industry giants, smaller companies
+            such as Alibaba and X (formerly Twitter) have also released their own powerful models, 
+            Qwen 3 Max and Grok 4, respectively. 
+            <br />
+            <br />
+            From March to November, 2025, more than ten new major AI models
+            were released, which showcases the rapid pace of innovation in this topic. Satya Nadella,
+            CEO of Microsoft, stated that as much as 30% of Microsoft's code is now being written
+            by AI, according to Novet & Vanian (2025). This highlights the significant impact these 
+            revolutionary technologies are having on existing industries, like software development. 
+            Not only are huge corporations adopting AI, however. According to Bick et al. (2025), from 
+            August 2024 to August 2025, adoption of AI by adults aged 18 to 64 rose from 44.6% to 54.6%.
+            This highlights that these revolutionary technologies are being utilized by individuals at
+            a growing rate as well.
+            <br />
+            <br />
+
           </p>
         </div>
         {/*Project Description*/}
         <div className="max-w-7xl" id="project-description">
-          <h3 className="pl-2 mb-2 text-2xl font-semibold">Project Description</h3>
+          <h3 className="pl-2 mt-6 mb-2 text-2xl font-semibold">Project Description</h3>
           <p className="pl-4 mb-2 text-lg">
-            This project is a full-stack web application built as a final project for ITIS 5166 Backend Application Development.
+            This project is a full-stack web application built as a final project for ITIS 
+            5166 Backend Application Development. This website was built with 4 pages: Login,
+            Dashboard, Summary, and Reports. Dashboard, Summary, and Reports are only visible
+            to those who are logged in, with authentication via JWT. The front end is made up
+            of React routes, utilizing multiple React components, with React Router to navigate.
+            MongoDB is used to host the data, while Python is leveraged to serve the data to the
+            frontend.To make this project a reality, I utilized the specific tech stack listed below.
           </p>
-          <h3 className="pl-2 mb-2 text-2xl font-semibold">Tech Stack</h3>
+          <h3 className="pl-2 mt-6 mb-2 text-2xl font-semibold">Tech Stack</h3>
           <ul className="mb-2 text-lg list-disc list-inside">
-            <li className="pl-4">Frontend - React</li>
+            <li className="pl-4">Frontend - React Router</li>
             <li className="pl-4">Styling - Tailwind CSS</li>
-            <li className="pl-4">Authentication - JWT</li>
-            <li className="pl-4">Backend - Python</li>
+            <li className="pl-4">Authentication - JWT (JOSE)</li>
+            <li className="pl-4">Backend - FastAPI (Python)</li>
             <li className="pl-4">Database - MongoDB</li>
+            <li className="pl-4">Hosting - DigitalOcean</li>
+            <li className="pl-4">AI Assistance - GPT-5 mini for code, Perplexity Pro/Max for implementation</li>
           </ul>
         </div>
       </section>
       <hr className="border-(--accent) border-2"/>
       <section className="py-6" id="quick-links">
         <h2 className="text-3xl font-semibold mb-4">Quick Links</h2>
-        <ul className="pl-4 flex space-x-6 text-lg">
+        <ul className="pl-4 flex space-x-6 text-lg items-center">
           <li>
-            <Link to="/summary" className="link">
-              View Summary
+            <Link to="/summary" className="button rounded transition px-4 py-2">
+              View Summary Page
             </Link>
           </li>
           <li>
-            <Link to="/reports" className=" hover:underline">
-              View Reports
+            <Link to="/reports" className="button rounded transition px-4 py-2">
+              View Reports page
             </Link>
           </li>
           <li>
-            <button onClick={handleSignOut} className=" hover:underline">
+            <Link to="/" onClick={handleSignOut} className="button rounded transition px-4 py-2">
               Sign out
-            </button>
+            </Link>
           </li>
         </ul>
       </section>
