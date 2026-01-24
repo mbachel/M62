0d65d52934c98765e8b9fa2afd7d9b2d0851dd58
mbachel <mattbachscouts@gmail.com>
2026-01-24 15:00:55 -0500

    feat!: rolling back to simplify project - too much complexity for complexity's sake

--- mongo-init/seed.sh
+++ mongo-init/seed.sh
@@ -6,15 +6,12 @@ sleep 5
 
 echo "Importing data into MongoDB..."
 
-# Import genai_adoption data
 mongoimport --host localhost --db m62 --collection genai_adoption \
     --file /docker-entrypoint-initdb.d/genai_adoption.json --jsonArray
 
-# Import model_performance data
 mongoimport --host localhost --db m62 --collection model_performance \
     --file /docker-entrypoint-initdb.d/model_performance.json --jsonArray
 
-# Import model_releases data
 mongoimport --host localhost --db m62 --collection model_releases \
     --file /docker-entrypoint-initdb.d/model_releases.json --jsonArray
 
